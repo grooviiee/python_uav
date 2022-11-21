0d2a2e4e28e4d87b152c35b894dd8feed5c0c761
grooviiee <ldj7202@gmail.com>
2022-11-22 00:54:49 +0900

    kickoff commit

---
+++ main_.py
@@ -0,0 +1,16 @@
+# import numpy
+# import json
+# import numpy as np
+# import cvxpy as cvx
+
+# f = np.array()
+# lb = np.array()
+# ub = np.array()
+
+# x = cvx.Variable()
+
+# print('Hello, world!')
+
+# with open('test.json') as f:
+#     json_data = json.load(f)
+# print(json.dumps(json_data))
