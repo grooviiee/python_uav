9bc997b7bf70ffdfc4e5e1466f05fb79895b596b
grooviiee <ldj7202@gmail.com>
2023-05-03 00:28:55 +0900

    Add reward function

--- uav/env/uavenv.py
+++ uav/env/uavenv.py
@@ -51,3 +51,45 @@ class Scenario(object):
         done = np.array([done] * self.num_agents)
         info = self._info_wrapper(info)
         return obs, reward, done, info
+    
+    # "UAV"
+    #     state = {x, y} for UAV , {x, y} for all users , {x_u,f} for all user and files
+    #     action = {y_m,f} for all files, {d_m}, {theta_m}, {power_m}
+    # "Master MBS"
+    #     state = {x, y} for BS , {x, y} for all users , {x, y} for UAVs
+    #     action = {z_u,i} which node connected with user
+    def reward(self, state, action):
+        # Receiving state and action as list 
+        epsilon = 0.2
+        # step 1. Get extr reward (Action에 대해서 state를 모두 바꾸었을 때, reward를 계산)
+        extr_reward = CalaExtrReward(self, state, action)
+        
+        # step 2. Get intr reward
+        intr_reward = CalcIIntrReward(self)
+
+        return (extr_reward + epsilon * intr_reward)
+    
+    def CalcExtrReward(self, status, action):
+        num_agent = len(status)
+        # step 1. action에 대한 status 변경 
+        for idx in num_agent:
+            if (idx == 0):
+                print("Process for MBS")
+                
+            else:
+                print("Process for UAV")
+            
+        # step 2. 변경된 status로 reward 계산
+        L = 100000000 # very large number
+        Delay = 0
+        for node in self.num_agent:
+            for user in self.num_user:
+                Delay += GetDelay()
+                
+        return L - Delay
+            
+    def GetDelay(self):
+        
+        
+        
+        
