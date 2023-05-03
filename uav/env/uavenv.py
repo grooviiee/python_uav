246f1f2140dc46a575a4826c915ccf8ad2b0faba
grooviiee <ldj7202@gmail.com>
2023-05-03 22:18:21 +0900

    Add Tx Rate calc function

--- uav/env/uavenv.py
+++ uav/env/uavenv.py
@@ -1,14 +1,28 @@
 import numpy as np
 from gym import spaces
-
+from uav.env.util import util
+import math
 
 # Refernece : C:\Users\June\Desktop\git\rl\maac\MAAC\envs\mpe_scenarios\fullobs_collect_treasure.py
 # UAV Environment scenario
+
+S = 10 * 1024 * 1024 # 10 Mbits
+B = 20*10^6
+W = 10*10^6
+MBS_POWER = 2 #Watt
+SPEED_OF_LIGHT = 3 * 10^8
+CARRIER_FREQEUENCY = 2*10^9
+QUOTA_UAV = 4
+QUOTA_MBS = 20
+PATHLOSS_EXP = 2
+NOISE_POWER = -100 #dB/Hz
 class Scenario(object):
     def __init__(self, args):
+        # parameter setting
         self.num_agents = args.num_agents
+        self.num_users = args.num_users
+        self.num_files = args.num_files
         self.scenario_name = args.scenario_name
-    
         self.action_space = []
         self.observation_space = []
         self.share_observation_space = []
@@ -34,12 +48,13 @@ class Scenario(object):
                     shape=self.env.observation_space.shape[1:],
                     dtype=self.env.observation_space.dtype
                 ))
-                
+
     def reset(self):
         obs = self.env.reset()
         obs = self._obs_wrapper(obs)
         return obs
 
+    # returns: next state, reward, done, etc.
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
         obs = self._obs_wrapper(obs)
@@ -84,12 +99,57 @@ class Scenario(object):
         Delay = 0
         for node in self.num_agent:
             for user in self.num_user:
-                Delay += GetDelay()
+                if node == 0:
+                    isMbs = False
+                else:
+                    isMbs = True
+                    
+                Delay += GetDelay(self, node, user, isMbs)
                 
-        return L - Delay
+        return (L - Delay)
             
-    def GetDelay(self):
+    def GetDelay(self, node, user, isMbs):
+        if isMbs == True: # for MBS
+            for file in self.num_files:
+                delay = self.x(user, file) * self.z(user, node) * self.T_down(node, user)
+        else: # for User
+            for file in self.num_files:
+                delay = self.x(user, file) * self.z(user, node) * {self.T_down(node, user) + (1 - self.y(node, file)) * self.T_back(node, user)}
         
+        return delay
+
+    def CalcT_down(self):
+        return S / R_2(i,u)
         
+    def CalcT_back(self):
+        return S / R_3(b,m,u)
+
+    def R_2(self, i, u):
+        numfile = 0
+        for file in self.num_files:
+            numfile += x(u, file) * z(u, i)
+            
+        upper = numfile * W
+        lower = 0
+        for user in self.num_users:
+            for file in self.num_files:
+                lower += x(u, file) * z(u, i)
+                
+        return (upper / lower * math.log2(1 + r(i, u)))
+    
+    def R_3(self, b, m, u):
+        left = math.log2(1 + r(b,m))
+        upper, lower = 0
+        for file in self.num_files:
+            upper += x(u, file) * z(u, m) * (1 - y(m, file))
+        upper *= B
         
-        
+        for user in self.num_users:
+            for file in self.num_files:
+                for node in self.num_agents:
+                    lower += x(user, file) * z(user, file) * (1 - y(node, file))
+                    
+        return (left * upper / lower)
+                    
+    def r(self, i , u):
+        NotImplemented
---
+++ uav/env/util.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# S = 10 * 1024 * 1024 # 10 Mbits
+# B = 20*10^6
+# W = 10*10^6
+# MBS_POWER = 2 #Watt
+# SPEED_OF_LIGHT = 3 * 10^8
+# CARRIER_FREQEUENCY = 2*10^9
+# QUOTA_UAV = 4
+# QUOTA_MBS = 20
+# PATHLOSS_EXP = 2
+
+# def CalcT_down():
+#     return S / R_2(i,u)
+    
+# def CalcT_back():
+#     return S / R_3(b,m,u)
+
+# def R_2(i, u):
+#     for env.files
