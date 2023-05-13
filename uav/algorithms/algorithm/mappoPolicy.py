22cc0643568ec815543b12438f1e9d8bc3680c2d
grooviiee <ldj7202@gmail.com>
2023-05-13 15:47:51 +0900

    Add some skeletons

--- uav/algorithms/algorithm/mappoPolicy.py
+++ uav/algorithms/algorithm/mappoPolicy.py
@@ -6,3 +6,25 @@ class MAPPOAgentPolicy:
         self.device = device
         self.lr = args.lr
         self.critic_lr = args.critic_lr
+        
+        self.actor = R_Actor()
+        self.cirtic = R_Critic()
+        
+        self.actor_optimizer = torch.optim.Adam()
+        self.critic_optimizer = torch.optim.Adam()
+        
+    def lr_decay(self, episode, num_episodes):
+        update_linear_schedule(self.actor_optimizer, episode, num_episodes, self.lr)
+        update_linear_schedule(self.critic_optimizer, episode, num_episodes, self.critic_lr)
+        
+    def get_actions:
+        NotImplemented
+        
+    def get_values:
+        NotImplemented
+        
+    def evaluate_actions:
+        NotImplemented
+    
+    def act:
+        NotImplemented
