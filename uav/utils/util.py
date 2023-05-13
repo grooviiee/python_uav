8495ffe37e7fc1e6c5a18d0782f1fdc73d0a04cf
grooviiee <ldj7202@gmail.com>
2023-05-13 15:25:29 +0900

    Add util files from reference repo

---
+++ uav/utils/util.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+import torch
+
+def get_shape_from_obs_space(obs_space):
+    if obs_space.__class__.__name__ == 'Box':
+        obs_shape = obs_space.shape
+    elif obs_space.__class__.__name__ == 'list':
+        obs_shape = obs_space
+    else:
+        raise NotImplementedError
+    return obs_shape
