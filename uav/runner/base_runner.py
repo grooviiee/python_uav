f0d83a91d1407e6c47b5b2aadc84ba26c9f9ad5f
grooviiee <86159019+grooviiee@users.noreply.github.com>
2023-05-17 23:32:18 +0900

    Update base_runner.py
    Add train function (refer separate leaning)

--- uav/runner/base_runner.py
+++ uav/runner/base_runner.py
@@ -14,3 +14,13 @@ class Runner(object):
     def compute(self):
         for agent_id in range(self.num_agents):
             next_value = self.trainer[agent_id].policy.get_value()
+            
+    def train(self):
+        train_info_list = []
+        for agent_id in range(self.num_uavs + self.num_mbs):
+            self.trainer[agent_id].prep_training()
+            train_info = self.trainer[agent_id].train(self.buffer[agent_id])
+            train_info_list.append(train_info)
+            self.buffer[agent_id].after_update()
+            
+        return train_info_list
