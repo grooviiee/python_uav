07cb4170735c1b45c78b299c6dbc0dba8ed1e106
grooviiee <ldj7202@gmail.com>
2023-05-18 22:10:32 +0900

    Start Fill mappo algorithm functions

--- uav/algorithms/mappo.py
+++ uav/algorithms/mappo.py
@@ -1,43 +1,35 @@
 import torch
-
+import numpy as np
+from algorithms.utils.util import check
 
 class MAPPOAgentTrainer:
-    def __init__(
-        self, args, obs_space, cent_obs_space, act_space, device  # =torch.device("gpu")
-    ):
+    def __init__(self,
+                 args,
+                 policy,
+                 device=torch.device("cpu")):
+
         self.device = device
-        self.lr = args.lr
-        self.critic_lr = args.critic_lr
-        self.opti_eps = args.opti_eps
-        self.weight_decay = args.weight_decay
-
-        self.obs_space = obs_space
-        self.share_obs_space = cent_obs_space
-        self.act_space = act_space
-
-    def get_action(self, cent_bos, obs, rnn_state_actor, rnn_state_critic):
-        """
-        Compute actions and value function predictions for the given inputs.
-        """
-        actions, action_log_probs, rnn_state_actor = self.actor()
-
-        value, rnn_state_critic = self.critic()
-
-        return value, action, action_log_prob, rnn_state_actor, rnn_state_critic
-
-    def act(self):
-        return NotImplemented
-
-	def train(self, buffer, update_actor=True):
+        self.tpdv = dict(dtype=torch.float32, device=device)
+        self.policy = policy
+                
+        self.ppo_epoch = args.ppo_epoch
+        self.num_mini_batch = args.num_mini_batch
         
+        self.data_chunk_length = args.data_chunk_length
+        self._use_recurrent_policy = args._use_recurrent_policy
+        self._use_naive_recurrent = args._use_naive_recurrent
+        self._use_max_grad_norm = args.use_max_grad_norm
+
+    # Train is acheived per Agent
+    def train(self, buffer, update_actor=True):
         advantages = buffer.returns[:-1] - buffer.value_preds[:-1]
         advantages_copy = advantages.copy()
         advantages_copy[buffer.active_masks[:-1] == 0.0] = np.nan
-        mean_advangates = np.nanmean(advantages_copy)
-        std_advantages = np.nanstd(advantages_copu)
+        mean_advantages = np.nanmean(advantages_copy)
+        std_advantages = np.nanstd(advantages_copy)
         advantages = (advantages - mean_advantages) / (std_advantages + 1e-5)
         
-        
+        # Create train_info
         train_info = {}
         train_info['value_loss'] = 0
         train_info['policy_loss'] = 0
@@ -46,18 +38,25 @@ class MAPPOAgentTrainer:
         train_info['critic_grad_norm'] = 0
         train_info['ratio'] = 0
         
+        # Start Train
         for _ in range(self.ppo_epoch):
+            if self._use_recurrent_policy:
+                data_generator = buffer.recurrent_generator(advantages, self.num_mini_batch, self.data_chunk_length)
+            elif self._use_naive_recurrent:
+                data_generator = buffer.naive_recurrent_generator(advantages, self.num_mini_batch)
+            else:
+                data_generator = buffer.feed_forward_generatro(advantages, self.num_mini_batch)    
+            
             for sample in data_generator:
                 
-                value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights \
-                = self.ppo_update(sample, update_actor)
+                value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights = self.ppo_update(sample, update_actor)
         	        
-        		train_info['value_loss'] += value_loss.item()
-        		train_info['policy_loss'] += policy_loss.item()
-        		train_info['dist_entropy'] += dist_entropy.item()
-        		train_info['actor_grad_norm'] += actor_grad_norm
-        		train_info['critic_grad_norm'] += critic_grad_norm
-        		train_info['ratio'] += imp_weights.mean()
+                train_info['value_loss'] += value_loss.item()
+                train_info['policy_loss'] += policy_loss.item()
+                train_info['dist_entropy'] += dist_entropy.item()
+                train_info['actor_grad_norm'] += actor_grad_norm
+                train_info['critic_grad_norm'] += critic_grad_norm
+                train_info['ratio'] += imp_weights.mean()
         	
             num_updates = self.ppo_epoch * self.num_mini_batch
             
@@ -66,7 +65,78 @@ class MAPPOAgentTrainer:
                 
             return train_info
         
+    def ppo_update(self, sample, update_actor=True):
+        # Update Actor and Critic Network
+        
+        # Step 1. Parse input data
+        share_obs_batch, obs_batch, rnn_states_batch, rnn_states_critic_batch, actions_batch, \
+            value_preds_batch, return_batch, masks_batch, active_masks_batch, old_action_log_probs_batch, \
+            adv_targ, available_actions_batch = sample
+        
+        old_action_log_probs_batch = check(old_action_log_probs_batch).to(**self.tpdv)
+        adv_targ = check(adv_targ).to(**self.tpdv)
+        value_preds_batch = check(value_preds_batch).to(**self.tpdv)
+        return_batch = check(return_batch).to(**self.tpdv)
+        active_masks_batch = check(active_masks_batch).to(**self.tpdv)
+
+        # Reshape to do in a single forward pass for all steps
+        values, action_log_probs, dist_entropy = self.policy.evaluate_actions(share_obs_batch,
+                                                                              obs_batch, 
+                                                                              rnn_states_batch, 
+                                                                              rnn_states_critic_batch, 
+                                                                              actions_batch, 
+                                                                              masks_batch, 
+                                                                              available_actions_batch,
+                                                                              active_masks_batch)
+        # Step 2. Actor update
+        imp_weights = torch.exp(action_log_probs - old_action_log_probs_batch)
+
+        surr1 = imp_weights * adv_targ
+        surr2 = torch.clamp(imp_weights, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv_targ
+
+        if self._use_policy_active_masks:
+            policy_action_loss = (-torch.sum(torch.min(surr1, surr2),
+                                             dim=-1,
+                                             keepdim=True) * active_masks_batch).sum() / active_masks_batch.sum()
+        else:
+            policy_action_loss = -torch.sum(torch.min(surr1, surr2), dim=-1, keepdim=True).mean()
+
+        policy_loss = policy_action_loss
+
+        self.policy.actor_optimizer.zero_grad()
+
+        if update_actor:
+            (policy_loss - dist_entropy * self.entropy_coef).backward()
+
+        if self._use_max_grad_norm:
+            actor_grad_norm = nn.utils.clip_grad_norm_(self.policy.actor.parameters(), self.max_grad_norm)
+        else:
+            actor_grad_norm = get_gard_norm(self.policy.actor.parameters())
+
+        self.policy.actor_optimizer.step()
+
+        # Step 3. Critic update
+        value_loss = self.cal_value_loss(values, value_preds_batch, return_batch, active_masks_batch)
+
+        self.policy.critic_optimizer.zero_grad()
+
+        (value_loss * self.value_loss_coef).backward()
+
+        if self._use_max_grad_norm:
+            critic_grad_norm = nn.utils.clip_grad_norm_(self.policy.critic.parameters(), self.max_grad_norm)
+        else:
+            critic_grad_norm = get_gard_norm(self.policy.critic.parameters())
+
+        self.policy.critic_optimizer.step()
+
+        return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights     
+    
         
         
-        
-        
+    def prep_training(self):
+        self.policy.actor.train()
+        self.policy.critic.train()
+
+    def prep_rollout(self):
+        self.policy.actor.eval()
+        self.policy.critic.eval()
--- uav/main.py
+++ uav/main.py
@@ -247,7 +247,7 @@ def parse_args():
     parser.add_argument("--value_loss_coef", type=float,
                         default=1, help='value loss coefficient (default: 0.5)')
     parser.add_argument("--use_max_grad_norm",
-                        action='store_false', default=True, help="by default, use max norm of gradients. If set, do not use.")
+                        action='store_false', default=False, help="by default, use max norm of gradients. If set, do not use.")
     parser.add_argument("--max_grad_norm", type=float, default=10.0,
                         help='max norm of gradients (default: 0.5)')
     parser.add_argument("--use_gae", action='store_false',
