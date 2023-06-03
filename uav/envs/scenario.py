c4a1a5ffa89a3c2760fa67854698375df4b0ccef
dongjune.lee <ldj7202@gmail.com>
2023-06-03 15:01:39 +0900

    Fix typo error.. num_files

--- uav/envs/scenario.py
+++ uav/envs/scenario.py
@@ -1,19 +1,20 @@
 import numpy as np
 from envs.core import World, Agent, User
 
+
 class BaseScenario(object):
     # Create Elements of the world.. It will be used as common settings
     def make_workd(self):
         raise NotImplementedError()
-    
+
     # create initial conditions of the world
     def reset_world(self, world):
         raise NotImplementedError()
 
     def info(self, agent, world):
         return {}
-    
-    
+
+
 class Scenario(BaseScenario):
     def make_world(self, args):
         world = World()
@@ -24,7 +25,7 @@ class Scenario(BaseScenario):
         world.num_users = args.num_users
         world.map_size = args.map_size
         world.num_files = args.num_files
-       # world.users = [User(args.file_size, args.zipf_parameter) for i in range(world.num_users)]
+        # world.users = [User(args.file_size, args.zipf_parameter) for i in range(world.num_users)]
         world.file_size = args.file_size
         world.zipf_parameter = args.zipf_parameter
         # Add agent as mbs
@@ -35,15 +36,16 @@ class Scenario(BaseScenario):
             world.agents.append(Agent(False))
         # Add user
         for i in range(world.num_users):
-            world.users.append(User(args.file_size, args.num_file, args.zipf_parameter))
-        
-        
+            world.users.append(
+                User(args.file_size, args.num_files, args.zipf_parameter)
+            )
+
         for i, agent in enumerate(world.agents):
-            agent.name = 'agent %d' % i
+            agent.name = "agent %d" % i
 
         for i, user in enumerate(world.users):
-            user.name = 'user %d' % i
-        
+            user.name = "user %d" % i
+
         # reset the world
         self.reset_world(world)
         return world
@@ -53,20 +55,20 @@ class Scenario(BaseScenario):
         world.assign_agent_colors()
         # random properties for landmarks
         world.assign_user_colors()
-        
+
         # 위치 조정 (randomly)
         for agent in world.agents:
             agent.x_loc = None
             agent.y_loc = None
-            
+
         for user in world.users:
             user.x_loc = None
             user.y_loc = None
-        
+
     def reward(self, agent, world):
         # It is used at uavenv.py
         NotImplemented
-        
+
     def observation(self, agent, world):
         # It is used at uavenv.py
         NotImplemented
