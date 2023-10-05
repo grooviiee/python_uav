318ce0851d715192efff2b14aa13c99e2fcd3c61
grooviiee <ldj7202@gmail.com>
2023-10-06 00:45:15 +0900

    add files to insert attention philosophy

--- algorithms/algorithm/atten_mappoPolicy.py
+++ algorithms/algorithm/atten_mappoPolicy.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
 from algorithms.algorithm.r_actor import R_Actor
-from algorithms.algorithm.r_critic import R_Critic
+from algorithms.algorithm.r_attention_critic import R_Critic
+# from algorithms.algorithm.r_critic import R_Critic
 
 class Attention_MAPPOAgentPolicy:
     def __init__(self, args, obs_space, cent_obs_space, act_space, agent_id, device=torch.device("cpu")):
@@ -21,6 +22,6 @@ class Attention_MAPPOAgentPolicy:
             self.is_uav = True
 
         self.actor = R_Actor(args, self.obs_space, self.act_space, self.is_uav, self.device)
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),                                                lr=self.lr, eps=self.opti_eps,                                                weight_decay=self.weight_decay)        
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.lr, eps=self.opti_eps, weight_decay=self.weight_decay)        
         self.critic = R_Critic(args, self.share_obs_space, self.is_uav, self.device)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),                                                 lr=self.critic_lr,                                                 eps=self.opti_eps, weight_decay=self.weight_decay)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.critic_lr, eps=self.opti_eps, weight_decay=self.weight_decay)
---
+++ algorithms/algorithm/r_attention.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, size):
+        super().__init__()
+        self.w_qs = nn.Conv2d(size, size, 1)
+        self.w_ks = nn.Conv2d(size, size, 1)
+        self.w_vs = nn.Conv2d(size, size, 1)
+
+        self.attention = ScaledDotProductAttention()
+
+    def forward(self, q, k, v):
+        residual = q
+        q = self.w_qs(q).permute(0, 2, 3, 1)
+        k = self.w_ks(k).permute(0, 2, 3, 1)
+        v = self.w_vs(v).permute(0, 2, 3, 1)
+
+        attention = self.attention(q, k, v).permute(0, 3, 1, 2)
+
+        out = attention + residual
+        return out
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, q, k, v):
+        attn = torch.matmul(q, k.transpose(2, 3))
+        output = torch.matmul(attn, v)
+
+        return output
