d782d6d10a9df47a68599549fc0b16e6fdab2a7f
grooviiee <ldj7202@gmail.com>
2023-10-15 23:15:37 +0900

    nn.Sequential in cnn looks out of order

--- algorithms/utils/cnn.py
+++ algorithms/utils/cnn.py
@@ -72,33 +72,16 @@ class CNNLayer(nn.Module):
                 init_(nn.Linear(hidden_size, 64)),
                 active_func,
         )
-        # self.cnn = nn.Sequential(
-        #     init_(
-        #         nn.Conv2d(      # 2D Convolution function
-        #             in_channels=input_channel,
-        #             #out_channels=hidden_size // 2,
-        #             out_channels=3,
-        #             kernel_size=kernel_size,    # it was set to 2
-        #             stride=stride,
-        #         )
-        #     ),
-        #     active_func,    # call nn.ReLU()
-        #     Flatten(),
-        #     init_(nn.Linear(num_hidden_layer, 64)),     # nn.Linear : Fully connected layer
-        #     active_func,
-        #     init_(nn.Linear(hidden_size, 64)),
-        #     active_func,
-        # )
 
         print(
             f"[INIT_CNN_LAYER] Init CNNLayer: [{input_channel},{input_width},{input_height}],{self.cnn}"
         )
 
     def forward(self, x):
-        print(f"[CNN_FORWARD]: (forward) input x: {x.shape}")
+        print(f"[CNN_FORWARD]: (before) input x({x.shape}): {x}")
         x = x / 255.0
         x = self.cnn(x)
-        print(f"[CNN_FORWARD]: (forward_after_self.cnn(x)) returned x({x.shape}): x")
+        print(f"[CNN_FORWARD]: (after.cnn(x)) returned x({x.shape}): {x}")
         return x
 
 
